c83e1cfdee6f1081be1912a039722611a2961719
luull <assalafy@steradian.co.id>
2025-03-03 08:57:48 +0700

    fix build

--- app/page.js
+++ app/page.js
@@ -125,7 +125,7 @@ export default function Home() {
           rel="noopener noreferrer"
           className="flex items-center my-2 gap-2 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-600 transition"
         >
-          <FaWhatsapp className="w-6 h-6" />
+          {/* <FaWhatsapp className="w-6 h-6" /> */}
           Lapor Bu Kelvy
         </a>
         {total > 0 && (
